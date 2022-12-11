ac8c3ae688640ae8b4c333dd2194a40956482233
Gustav_o <jandersongusta@gmail.com>
2022-12-11 12:02:51 -0300

    Acabou!!!!!!!!!!!!!!!!!!!!!!!

--- ifEvents/app/Models/Event.php
+++ ifEvents/app/Models/Event.php
@@ -18,9 +18,10 @@ class Event extends Model
     protected $guarded = [];
 
     public function user() {
-
         return $this->belongsTo('App\Models\User');
-        
     }
 
+    public function users() {
+        return $this->belongsToMany('App\Models\User');
+    }
 }
